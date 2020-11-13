26572ffebf3bb396c760bacf1efd370dd321b911
Cynthia Kitili <nyambucindy0@gmail.com>
2020-11-14 02:18:23 +0300

    add user service

--- src/app/user-service/user.service.ts
+++ src/app/user-service/user.service.ts
@@ -1,14 +1,9 @@
 import { Injectable } from '@angular/core';
 
-
 @Injectable({
   providedIn: 'root'
 })
-export class GitService {
-  getGitUserName(){
-    return GitRepos
-  }
-
+export class UserService {
 
   constructor() { }
 }
