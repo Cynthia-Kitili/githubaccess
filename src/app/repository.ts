a4fdd75c83fefa6d8d159e61812ec64961e80c05
Cynthia Kitili <nyambucindy0@gmail.com>
2020-11-13 19:53:28 +0300

    add changes made to user class

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,3 +1,3 @@
 export class Repository {
-    name: string;
+    gitRepos: string;
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,3 +1,9 @@
 export class User {
-   name: string;
+    created_at: string;
+    avatar_url: any;
+    followers_url: number;
+    following_url: number;
+    repos_url: string;
+    login: number;
+    public_repos: string;
 }
