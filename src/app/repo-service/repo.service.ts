a6cc754275c1465582d0db421f7e009a6298fabb
Cynthia Kitili <nyambucindy0@gmail.com>
2020-11-14 14:43:53 +0300

    add code in repo service

--- src/app/repo-service/repo.service.ts
+++ src/app/repo-service/repo.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepoService {
+  url = 'https://api.github.com/users/';
+  token = '?access_token= 7676941243a5947d9b71b06171ac26b88099d860 ';
 
-  constructor() { }
+  constructor(public http: HttpClient) {
+
+   }
+   getRepo(search:string): Observable<any> {
+    return this.http.get(this.url + search + '/repos?' + this.token);
+  }
+}
 }
