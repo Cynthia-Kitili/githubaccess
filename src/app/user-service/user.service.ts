9997e63e628d82e22482b1e08fefaa33f5b10c4e
Cynthia Kitili <nyambucindy0@gmail.com>
2020-11-14 14:57:12 +0300

    add search component

--- src/app/user-service/user.service.ts
+++ src/app/user-service/user.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  user: User[]=[];
+  url = 'https://api.github.com/users/';
+  token = '?access_token= 7676941243a5947d9b71b06171ac26b88099d860 ';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
 }
