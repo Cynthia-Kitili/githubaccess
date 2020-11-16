8b048ab8cc413ce17129e6c19d14831103ca9f6b
Cynthia Kitili <nyambucindy0@gmail.com>
2020-11-17 01:12:53 +0300

    add pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { UserComponent } from './user/user.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import {FormsModule} from '@angular/forms'
+import {FormsModule} from '@angular/forms';
+import { DatePipe } from './date.pipe'
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
     UserComponent,
-    NavbarComponent
+    NavbarComponent,
+    DatePipe
   ],
   imports: [
     BrowserModule,
